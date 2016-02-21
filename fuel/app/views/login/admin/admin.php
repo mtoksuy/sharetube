7202c2550a055bed88fd802faff7e4580a57b57b
mtoksuy <mtoksuy@programmerbox.com>
2016-02-22 03:14:13 +0900

    admin更新

--- fuel/app/views/login/admin/admin.php
+++ fuel/app/views/login/admin/admin.php
@@ -4,7 +4,7 @@
 					<?php echo '<h2 class="h2_heading_1 m_0 m_b_30">ようこそ'.$_SESSION["sharetube_id"].'さん</h2>'; ?>
 
 					<div class="m_b_30">
-						<p>Sharetube version 5.4.11<br>
+						<p>Sharetube version 5.4.12<br>
 <!--※お手数かけますが、新機能をご利用するためにこのページと「まとめ作成」ページにてスーパーリロードお願い致します。</p>
 -->
 
@@ -62,6 +62,19 @@
 	<div class="matome_content_block_timeline">
 		<ol>
 
+
+
+
+			<li class="clearfix">
+				<dl>
+					<dt>
+						<pre>2016年02月22日：Var.5.4.12</pre>
+					</dt>
+					<dd>
+						<pre>・インセンティブ周りメールレポート実装</pre>
+					</dd>
+				</dl>
+			</li>
 
 
 			<li class="clearfix">
