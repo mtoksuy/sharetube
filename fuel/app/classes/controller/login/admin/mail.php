ec6f54c8e8e54c832646d6b92f95a613483296b9
mtoksuy <mtoksuy@programmerbox.com>
2016-05-03 22:26:05 +0900

    version_5.4.16 バグフィックス

--- fuel/app/classes/controller/login/admin/mail.php
+++ fuel/app/classes/controller/login/admin/mail.php
@@ -11,7 +11,6 @@
 	// ルーター
 	public function router($method, $params) {
 		$params = $params[0];
-//		var_dump('aaaaaaaaaaaaaaaaaa '.$method, $params);
 		$mail_array_data = array();
 		// メール送信画面表示
 		if($method == 'index') {
@@ -28,19 +27,22 @@
 		// ログインチェック
 		$login_check = Model_Login_Basis::login_check();
 		if($login_check) {
-			// viewテンプレート読み込み
-			$this->login_admin_template            = View::forge('login/admin/template');
-			$this->login_admin_template->view_data = array(
-				'title'   => 'メール｜アドミン｜ログイン|'.TITLE,
-				'content' => View::forge('login/admin/list/list'),
-			);
-
-			// コンテンツ挿入
-			$this->login_admin_template->view_data["content"]->set('content_data',array(
-				'content_html' => View::forge('login/admin/mail/mail'),
-			),false);
-			return $this->login_admin_template;
-		}
+			// マスターだけ操作可能
+			if($_SESSION["sharetube_id"] == 'mtoksuy') {
+				// viewテンプレート読み込み
+				$this->login_admin_template            = View::forge('login/admin/template');
+				$this->login_admin_template->view_data = array(
+					'title'   => 'メール｜アドミン｜ログイン|'.TITLE,
+					'content' => View::forge('login/admin/list/list'),
+				);
+	
+				// コンテンツ挿入
+				$this->login_admin_template->view_data["content"]->set('content_data',array(
+					'content_html' => View::forge('login/admin/mail/mail'),
+				),false);
+				return $this->login_admin_template;
+			}
+		} // if($login_check) {
 			// ログインしていない場合
 			else {
 				header('Location: '.HTTP.'');
