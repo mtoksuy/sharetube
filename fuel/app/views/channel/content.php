580e405fe8bf2e7df4af964b545647a2d097c868
mtoksuy <mtoksuy@programmerbox.com>
2017-03-24 16:38:19 +0900

    version_5.4.28 微調整

--- fuel/app/views/channel/content.php
+++ fuel/app/views/channel/content.php
@@ -1,3 +1,6 @@
+<div class="card_article_header">
+						<span class="typcn typcn-document-text"></span><span>作成まとめ</span>
+					</div>
 	<?php echo $content_data["content_html"]; ?>
 
 	<?php echo $content_data["paging_html"]; ?>
