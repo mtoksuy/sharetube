ec390641465c6acbf2a727d387f4f576ad8ec4a0
mtoksuy <mtoksuy@programmerbox.com>
2017-08-10 18:35:06 +0900

    version_5.5.3 オーバーレイ変更

--- fuel/app/views/basic/mobilead.php
+++ fuel/app/views/basic/mobilead.php
@@ -1,10 +1,18 @@
 
-<?php
+<?php 
 		// 広告配信
 		$detect  = Model_info_Basis::mobile_detect_create();
 		// Fluct広告
 //		$ad_mobile_deader_html   = Model_Ad_Html::fluct_ad_html_create($detect, 'none', 'ヘッダー');
-		$ad_mobile_orverlay_html = Model_Ad_Html::fluct_ad_html_create($detect, 'none', 'オーバーレイ');
+//		$ad_mobile_orverlay_html = Model_Ad_Html::fluct_ad_html_create($detect, 'none', 'オーバーレイ');
+
+		// 全ての広告別array取得
+		$all_ad_html_array = Model_Ad_Html::all_ad_html_array_get();
+		// アドネットワークをランダムで取得
+		$ad_network_name = Model_Ad_Basis::ad_network_random_get(array('fluct', 'geniee'));
+		// 広告ネットワーク指定アドhtml生成
+		$ad_mobile_orverlay_html   = Model_Ad_Html::all_ad_html_create($all_ad_html_array, $detect, 'fluct', $ad_network_name, 'none', 'オーバーレイ');
+
 		// 2月20日スタート 3月30日エンド
 //		$ad_mobile_orverlay_html = Model_Ad_Html::techno_arcadia_ad_html_create($detect, 'none', 'オーバーレイ');
 //		var_dump($ad_mobile_orverlay_html);
